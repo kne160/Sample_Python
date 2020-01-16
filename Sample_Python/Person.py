10f6ebd7ae696d8d8da23182989805f75fa26200
kne16 <kne16@ideapad320S>
2020-01-16 13:23:31 +0900

    1st commit

---
+++ Sample_Python/Person.py
@@ -0,0 +1,21 @@
+'''
+Sample Class
+
+@author: kne16
+'''
+
+class Person():
+    def __init__(self, name):
+        self.name = name
+
+class Student(Person):
+    def __init__(self, name, id):
+        super().__init__(name)
+        self.name = name
+        self.id = id
+
+person = Person('I am a person')
+student = Student('I am a student', 777)
+
+print(person.name)
+print(student.name, student.id)
