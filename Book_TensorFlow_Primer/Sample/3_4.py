bb9af7904a2fffc74cdbe4ad19f4278b22ff3dfc
kne16 <kne16@ideapad320S>
2020-01-19 18:44:43 +0900

    a

---
+++ Book_TensorFlow_Primer/Sample/3_4.py
@@ -0,0 +1,14 @@
+'''
+Created on 18 Jan 2020
+
+@author: kne16
+'''
+import tensorflow as tf
+# import tflearn
+# 
+# import tflearn.datasets.mnist as mnist
+ 
+from matplotlib import pyplot as plt
+from matplotlib import cm
+import numpy as np
+
---
+++ Book_TensorFlow_Primer/Sample/Test.py
@@ -0,0 +1,10 @@
+'''
+Created on 18 Jan 2020
+
+@author: kne16
+'''
+
+import tensorflow as tf
+
+print(tf.__version__)
+print(tf.keras.__version__)
