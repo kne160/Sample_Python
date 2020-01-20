6d1819269d1e57b5db0e13a69c90428fa701f742
kne16 <kne16@ideapad320S>
2020-01-20 15:42:00 +0900

    a

--- Book_TensorFlow_Primer/Sample/3_4.py
+++ Book_TensorFlow_Primer/Sample/3_4.py
@@ -4,9 +4,8 @@ Created on 18 Jan 2020
 @author: kne16
 '''
 import tensorflow as tf
-# import tflearn
-# 
-# import tflearn.datasets.mnist as mnist
+import tflearn
+import tflearn.datasets.mnist as mnist
  
 from matplotlib import pyplot as plt
 from matplotlib import cm
